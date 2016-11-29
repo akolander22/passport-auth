20c6c853d0ab085063683f9dbf502de64bc66f67
Andrew Kolander <akolander22@gmail.com>
2016-11-29 11:04:58 -0600

    working local login

--- app/routes.js
+++ app/routes.js
@@ -6,7 +6,17 @@ module.exports = function(app, passport) {
     });
 
     // route for login form
+    app.get('/login', function(req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.render('login.ejs', { message: req.flash('loginMessage') });
+    });
     // route for processing the login form
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
     // route for signup form
     app.post('/signup', passport.authenticate('local-signup', {
         successRedirect : '/profile', // redirect to the secure profile section
@@ -14,6 +24,11 @@ module.exports = function(app, passport) {
         failureFlash : true // allow flash messages
     }));
     // route for processing the signup form
+    app.get('/signup', function(req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.render('signup.ejs', { message: req.flash('signupMessage') });
+    });
 
     // route for showing the profile page
     app.get('/profile', isLoggedIn, function(req, res) {
